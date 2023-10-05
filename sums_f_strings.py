5f543e4ae44a151efecce5bbc832344294f201f7
utdictum <natgeomara@gmail.com>
2023-10-05 16:47:13 +0300

    start

---
+++ sums_f_strings.py
@@ -0,0 +1,10 @@
+"""Please write a program which asks the user for four numbers. The program then prints out the sum and the mean of the numbers.
+
+The program should function as follows:
+
+Sample output
+Number 1: 2
+Number 2: 1
+Number 3: 6
+Number 4: 7
+The sum of the numbers is 16 and the mean is 4.0"""
