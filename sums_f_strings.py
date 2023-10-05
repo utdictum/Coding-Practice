3f1c4ee5e4842c332fbaf073854ec796cb9f4016
utdictum <natgeomara@gmail.com>
2023-10-05 16:50:12 +0300

    more concise sum and mean for inputs

--- sums_f_strings.py
+++ sums_f_strings.py
@@ -8,7 +8,8 @@ Number 2: 1
 Number 3: 6
 Number 4: 7
 The sum of the numbers is 16 and the mean is 4.0"""
-sum = 0
+
+"""sum = 0
 
 sum += int(input("Number 1: "))
 sum += int(input("Number 2: "))
@@ -17,4 +18,11 @@ sum += int(input("Number 4: "))
 
 mean = sum / 4
 
-print(f"The sum of the numbers is {sum} and the mean is {mean}")
+print(f"The sum of the numbers is {sum} and the mean is {mean}")"""
+#better
+sum = 0
+
+for i in range(1, 5):
+    sum += int(input(f"Number {i}: "))
+
+print(f"The sum of the numbers is {sum} and the mean is {sum / 4}")
