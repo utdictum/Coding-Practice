e8e3f9a1f15ae6a0369c2479ab492771e6e8b0b8
utdictum <natgeomara@gmail.com>
2023-10-05 18:10:01 +0300

    proving formula

--- ceil_function.py
+++ ceil_function.py
@@ -13,11 +13,17 @@ Desired group size? 3
 Number of groups formed: 4 """
 students = int(input("How many students on the course? "))
 desired_group_size = int(input("Desired group size? "))
-#number of full groups 
+#number of 
 full_groups = students // desired_group_size
-"""if a//b a//b + b-1//b results ub b-1//b = 0 
-if a !// b a//b+ int+b-1 then 2b>b-1>=b
+"""if a//b  then (a+b-1) //b=a//b+(b-1)b
+because a//b+b-1//b b-1//b=0
+if a !// b 
+a=cb+r
+a+b-1=cb+r+b-1=cb+(r+b-1)
+a//b+ int+b-1 then 2b>b-1>=b
+c+b-1<2b
 all devided by b
-2>b-1/b >=1 """
+2>b-1/b >=1 
+"""
 full_groups += (students % desired_group_size + desired_group_size - 1) // desired_group_size
 print(f"Number of groups formed: {full_groups}")
